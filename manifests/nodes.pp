0c163082e3f586111ad2e862b685877d6a80c0b9
Lin He <he9lin@gmail.com>
2012-04-30 23:56:54 -0700

    Add mysql module and test success

--- manifests/nodes.pp
+++ manifests/nodes.pp
@@ -2,13 +2,54 @@ node 'app' {
   include stdlib
   include nginx
   include redis
-  include mongodb
+  # include mongodb
   include nodejs
   include railsapp
 
   $app_name = 'jieqoo.com'
+  $app_db   = 'jieqoo_production'
   
   railsapp::resource::location { $app_name: }
+  
+  
+  
+  ## Mysql stuff
+  
+  include mysql
+  include mysql::ruby
+  
+  # Need to set your password here: /etc/mysql/my.cnf
+  # [client]
+  # password        = YOUR_PASSWORD
+  
+  class { 'mysql::server':
+    config_hash => {'root_password' => 'KassJieqoo4ever'}
+  }
+  
+  mysql::db { $app_db:
+    user     => 'vagrant',
+    password => 'Kass4ever',
+    charset  => 'utf8',
+    host     => 'localhost',
+    grant    => ['all'],
+    require  => Class['mysql::server']
+  }
+
+  # database { $app_db :
+  #   ensure  => present,
+  #   charset => 'utf8',
+  #   require => Class['mysql::server'],
+  # }
+
+  # database_user { 'vagrant@localhost':
+  #   ensure        => present,
+  #   password_hash => mysql_password('Kass4ever'),
+  #   require       => Class['mysql::server'],
+  # }
+  
+  
+  
+  ## Nginx stuff
     
   nginx::resource::upstream { 'rails_server':
       ensure  => present,
